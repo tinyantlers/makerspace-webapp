0716315defc02fed0d7456b201fa207d7592a10a
Alexis Ugalde <alex@Alexiss-MacBook-Pro.local>
2021-12-03 16:36:28 -0800

    my updates

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+
+  );
+}
+
+export default Home;
